88b5acc6525069a453d7528b935c4e0b42557c76
Mahdi Karimian <mehdi.karimian.777@gmail.com>
2023-07-31 13:11:09 +0200

    final calculator

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 
-/* const zero = document.querySelector(".zero")
-const one = document.querySelector(".one")
-const two = document.querySelector(".two")
-const three = document.querySelector(".three")
-const four = document.querySelector(".four")
-const five = document.querySelector(".five")
-const six = document.querySelector(".six")
-const seven = document.querySelector(".seven")
-const eight = document.querySelector(".eight")
-const nine = document.querySelector(".nine") */
 const number = document.querySelector(".number")
 let reserve = 0;
-let buffer = "0";
-
+let buffer = "0"
+let intBuffer;
+let runningTotal = 0;
+let previousOperator = null;
 let screen = document.getElementById("result")
 const clear = document.getElementById("clear")
 const multiple = document.getElementById("multiple")
@@ -46,37 +38,63 @@ function handleNumber (number) {
 function handleSymbol (symbol) {
     switch (symbol) {
         case 'C':
-            console.log("c") ;
             buffer = "0";
             break;
         case '=' :
-            console.log("equal") ;
+            if(previousOperator === null) {
+                /* do nothing */
+                return
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = '' + runningTotal;
+            runningTotal = 0;
             break;   
         case '<' :
-            console.log("<") ;
+            if (buffer.length === 1) {
+                buffer = "0"
+            } else {
+                buffer = buffer.substring(0 , buffer.length - 1)
+            }
             break;  
         case '+' :
         case '-' :
-        case '*' : 
-        case '/' :
-            console.log("math") ;
+        case '×' : 
+        case '÷' :
+            handleMath (symbol)
             break;  
     } 
 
-
-    if (symbol === "+") {
-        addFunc(symbol)
-        screen.innerText = ''
-    }
 }
 
+/* handle math */
 
+function handleMath (value) {
+    if (buffer === '0'){
+        return /* do nothing */
+    }
+    intBuffer = parseInt(buffer);
+    
+    if  (runningTotal === 0) {
+        runningTotal = intBuffer;
+        
+    } else {
+        flushOperation (intBuffer)
+    }
+    previousOperator = value;
+    buffer = '0';
+}
 
-/* add */
-function addFunc (add)  {
-    console.log(add)
-    firstNum = screen.innerText
-    console.log(firstNum)
+function flushOperation (intBuffer) {
+    if (previousOperator === '+') {
+        runningTotal += intBuffer
+    } else if (previousOperator === '-') {
+        runningTotal -= intBuffer
+    } else if (previousOperator === '×') {
+        runningTotal *= intBuffer
+    } else if (previousOperator === '÷') {
+        runningTotal /= intBuffer
+    }
 }
 
 /* rendering result on every click */
